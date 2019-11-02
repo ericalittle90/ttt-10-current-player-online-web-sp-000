61cf0b948c9bd3cc6383e060ea0f9548bcb17469
Erica Little <eteney90@gmail.com>
2019-11-02 18:04:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ return board
 end
 
 def current_player(board)
-if turn_count % 2 == 0
+if turn_count(board) % 2 == 0
 return "X"
-else turn_count % 2 != 0
+else turn_count(board) % 2 != 0
 return "O"
 end
 end
